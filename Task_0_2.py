365cd25bb97b26db4e9f13d3f401029c68bdda32
ChernovIvan86 <4ern86@bk.ru>
2023-05-08 23:18:42 +0300

    Task_0_2.py

---
+++ Task_0_2.py
@@ -0,0 +1,7 @@
+print('     ************\n   СРАВНИВАНИЕ ЧИСЕЛ')
+x=float(input('ВВЕДИТЕ X:',))
+y=float(input('ВВЕДИТЕ Y:',))
+
+if x>y: print(x,' - больше')
+elif x<y: print(y,' - больше')
+else: print('Вы ввели равные числа')
