4003c7a8aa3929dd68f2ca3d35e3a0fdadf04486
ChernovIvan86 <4ern86@bk.ru>
2023-05-09 22:31:16 +0300

    Task_3_2.py

--- workout.py
+++ workout.py
@@ -3,3 +3,11 @@ print('123'+'123')
 print(print(123))
 print(123,'123')
 print(print)
+
+                       # задать строку st, содержащую: 112225567789
+dct={1:3, 2:4, 3:5}                                      # задать пустой словарь
+for i in dct:
+    dct[i]=dct.get(i, 0)+1                      #
+    print(i, '-', dct[i], sep=' ', end='\n')
+
+print(dct)
