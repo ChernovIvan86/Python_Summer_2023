fa2f5fcdf2389a89f5e29656db6d79ca723e04c7
ChernovIvan86 <4ern86@bk.ru>
2023-05-09 15:22:21 +0300

    Task_2_2.py

--- Task_2_2.py
+++ Task_2_2.py
@@ -1,4 +1,4 @@
-# Способ I
+                            # Способ I
 lst=[1, 2, 3]
 
 a=int(lst[0])
@@ -9,16 +9,35 @@ if a<b and a<c : print(a, '- меньше всех', end='\n')
 if b<a and b<c : print(b, '- меньше всех', end='\n')
 if c<a and c<b : print(c, '- меньше всех', end='\n')
 
-# Способ II
+                            # Способ II
 lst=[]
 n=(int(input('Введите число элементов списка: ')))
 lst.append(int(input('Введите НОВЫЙ элемент списка: ')))
 mi=int(lst[0])
-mi_index=0
+index_mi=0
+
 for i in range(1,n):
     lst.append(int(input('Введите НОВЫЙ элемент списка: ')))
     if mi>int(lst[i]):
         mi=int(lst[i])
-        mi_index=mi_index+1
-print('элемент', mi, 'с индексом', mi_index,'оказался меньше всех')
+        index_mi = i
+print('элемент', mi, 'с индексом', index_mi,'оказался меньше всех')
 
+                            # Способ III
+lst=[]
+n=(int(input('Введите число элементов списка: ')))
+lst.append(int(input('Введите НОВЫЙ элемент списка: ')))
+
+for i in range(1,n):
+    lst.append(int(input('Введите НОВЫЙ элемент списка: ')))
+
+print('Вы ввели список: ', lst)
+
+mi = int(lst[0])
+index_mi=0
+
+for i in range(1,n):
+    if mi>int(lst[i]):
+        mi=int(lst[i])
+        index_mi=i
+print('элемент', mi, 'с индексом', index_mi,'оказался меньше всех')
