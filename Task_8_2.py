ea3142a97fb9c1627c6f83c991f6ead592e75942
ChernovIvan86 <4ern86@bk.ru>
2023-05-19 18:22:52 +0300

    Task_8_2.py

--- Task_8_2.py
+++ Task_8_2.py
@@ -5,22 +5,33 @@
 # Каждый подсписок отсортировать по убыванию.
 
 lst=[[1,5,3], [2,44,1,4], [3,3], [3,33,1,4]]
+lst_unter=[]
+st=''
 
+# Сортировка каждого подсписка по убыванию
 for i in lst:
     i.sort(reverse=True)
 print(lst)
 
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!->
-#lst.sort(key=map(lambda x: len("".join(str(x))), lst))
-# <-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+for i in range(0, len(lst)):
+    for j in range(0, len(lst[i])):
+        st=st+str(lst[i][j])
+    lst_unter.append(st)
+    st =''
+print(lst_unter)
 
+for i in range(0, len(lst_unter)):
+    lst_unter[i]=len(lst_unter[i])
+print(lst_unter)
 
-
+lst=list(zip(lst_unter, lst))
 print(lst)
 
-print("".join(map(str, lst))) # map() приводит все элементы списка к строке
-                                     # join() склеивает их в одну строку (""-без пробелов)
+lst=sorted(lst)
+print(lst)
 
-
-
+lst_unter=[]
+for i in range(0, len(lst)):
+    lst_unter.append(lst[i][1])
+print(lst_unter)
