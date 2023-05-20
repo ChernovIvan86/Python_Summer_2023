7d8e11de9bca2c8f8482201d1846bf3e38d5589b
ChernovIvan86 <4ern86@bk.ru>
2023-05-20 16:43:16 +0300

    Task_8_3.py

--- Task_8_3.py
+++ Task_8_3.py
@@ -7,47 +7,12 @@
     # Выход: ['abcbab', 'abab', 'aaaaaaaa', 'xx'].
 
 lst=['abab', 'xx', 'aaaaaaaa', 'abcbab']
-lst.sort()
-print(lst)
 
-tes={}
-lst_unique=[]
-lst_num=[]
-su=[]
-
-print(lst_unique)
-
-for i in range(0, len(lst)):
-    tes=set(lst[i])
-    lst_unique.append(tes)
-    lst_num.append(len(tes))
-    tes = {}
-su=list(zip(lst_num, lst))
-
-print(lst_unique)
-print(lst_num)
-print(su)
-print()
-
-# сортировка элементов su по алфавиту 2х элементов картежей
-su.sort(key=lambda a: a[1])
-print(su)
-
-# сортировка элементов su по убывнию 1х элементов картежей
-su=sorted(su, reverse=True)
-print(su)
-
-
-
-
+lst=sorted(lst, key=lambda x: (-len(set(x)), x))
 
 # печать отсортированного списка из списка картежей
-lst = []
-for i in range(0, len(su)):
-    lst.append(su[i][1])
 print(*lst)
 
-print('xx'>'aaaa')
 
 
 
